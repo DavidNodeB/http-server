f5c3e7be77a165d55406e61c5b8b50541adf14f6
DavidNodeB <db1416db@gmail.com>
2024-12-19 17:03:44 -0500

    Added client and server messages

--- src/client.c
+++ src/client.c
@@ -1,12 +1,17 @@
 #include <stdio.h>
 #include <winsock2.h>
+#include <string.h>
+#define BUFFER_SIZE 512
 
 int main() {
     WSADATA wsaData;
-    SOCKET clientSocket; 
+    SOCKET clientSocket;
     struct sockaddr_in serverAddr;
     char *serverIP = "127.0.0.1";
-    int port = 5001; 
+    char *clMessage = "CLIENT: message received from server";
+    int clMessageLen = strlen(clMessage);
+    char clbuffer[BUFFER_SIZE]; // default size
+    int port = 5001;
 
     if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
         printf("CLIENT_ERROR: %d\n", WSAGetLastError());
@@ -24,13 +29,34 @@ int main() {
     serverAddr.sin_port = htons(port);
 
     if (connect(clientSocket, (struct sockaddr *)&serverAddr, sizeof(serverAddr)) == SOCKET_ERROR) {
-        printf("CLIENT_ERROR: Connection failed.\n");
+        printf("CLIENT_ERROR: %d\n", WSAGetLastError());
+        closesocket(clientSocket);
         WSACleanup();
         return 1;
     }
 
     printf("CLIENT: connected to server.\n");
 
+    if (send(clientSocket, clMessage, clMessageLen, 0) == SOCKET_ERROR) {
+        printf("CLIENT_ERROR: %d\n", WSAGetLastError());
+        closesocket(clientSocket);
+        WSACleanup();
+        return 1;
+    }
+
+    printf("CLIENT: message sent\n");
+
+    // Correct recv buffer size
+    if (recv(clientSocket, clbuffer, BUFFER_SIZE, 0) == SOCKET_ERROR) {
+        printf("SERVER_ERROR: %d\n", WSAGetLastError());
+        closesocket(clientSocket);
+        WSACleanup();
+        return 1;
+    }
+
+    printf("CLIENT: received message: %s\n", clbuffer);
+
+    closesocket(clientSocket);
     WSACleanup();
     getchar(); 
     return 0;
--- src/server.c
+++ src/server.c
@@ -1,66 +1,78 @@
 #include <stdio.h>
 #include <winsock2.h>
+#include <string.h>
 
-int main(int argc, char *argv[])
-{
-    WSADATA wsaData; 
-    SOCKET serverSocket; 
-    struct sockaddr_in serverAddr;  
+int main(int argc, char *argv[]) {
+    WSADATA wsaData;
+    SOCKET serverSocket, clientSocket;
+    struct sockaddr_in serverAddr, clientAddr;
     WORD ws2Version = MAKEWORD(2, 2);
     char *serverIP = "127.0.0.1"; // localhost
     int serverSize = sizeof(serverAddr); // size of serverAddr in bytes
-    int port = 5001; 
+    char *message = "Hello client from server";
+    int messageLen = strlen(message);
+    char recvBuffer[512];
+    int clientSize = sizeof(clientAddr);
+    int port = 5001;
 
     if (WSAStartup(ws2Version, &wsaData) != 0) {
-        printf("SERVER_ERROR: %d\n", WSAGetLastError()); 
-        WSACleanup();
+        printf("SERVER_ERROR: %d\n", WSAGetLastError());
         return 1; // error and exit
     }
 
-    printf("SERVER: socket initialized\n"); 
+    printf("SERVER: socket initialized\n");
 
-    if ((serverSocket = socket(AF_INET, SOCK_STREAM, 0)) == SOCKET_ERROR)
-    {
-        printf("SERVER_ERROR: %d\n", WSAGetLastError()); 
-        WSACleanup(); 
+    if ((serverSocket = socket(AF_INET, SOCK_STREAM, 0)) == SOCKET_ERROR) {
+        printf("SERVER_ERROR: %d\n", WSAGetLastError());
         return 1;
     }
 
-    printf("SERVER: socket created\n"); 
+    printf("SERVER: socket created\n");
 
-    serverAddr.sin_family = AF_INET; 
+    serverAddr.sin_family = AF_INET;
     serverAddr.sin_addr.s_addr = htonl(INADDR_ANY); // accept any incoming connections
-    serverAddr.sin_port = htons(port); 
+    serverAddr.sin_port = htons(port);
 
-    if (bind(serverSocket, (struct sockaddr*)&serverAddr, sizeof(serverAddr)) == SOCKET_ERROR)
-    {
-        printf("SERVER_ERROR: %d\n", WSAGetLastError()); 
-        return 1; 
+    if (bind(serverSocket, (struct sockaddr*)&serverAddr, sizeof(serverAddr)) == SOCKET_ERROR) {
+        printf("SERVER_ERROR: %d\n", WSAGetLastError());
+        return 1;
     }
 
-    printf("SERVER: socket binded\n");  
+    printf("SERVER: socket binded\n");
 
-    if (listen(serverSocket, 5) == SOCKET_ERROR)
-    {
-        printf("SERVER_ERROR: %d\n", WSAGetLastError()); 
-        WSACleanup();
-        return 1; 
+    if (listen(serverSocket, 5) == SOCKET_ERROR) {
+        printf("SERVER_ERROR: %d\n", WSAGetLastError());
+        return 1;
     }
 
-    printf("SERVER: socket listening\n"); 
+    printf("SERVER: socket listening\n");
 
-    if (accept(serverSocket, (struct sockaddr *)&serverAddr, &serverSize) == INVALID_SOCKET)
-    {
-        printf("SERVER_ERROR: %d\n", WSAGetLastError()); 
-        WSACleanup(); 
-        return 1; 
+    // need clientSocket or else you can't send message or receive
+    if ((clientSocket = accept(serverSocket, (struct sockaddr*)&clientAddr, &clientSize)) == INVALID_SOCKET) {
+        printf("SERVER_ERROR: %d\n", WSAGetLastError());
+        return 1;
     }
 
-    printf("SERVER: client accepted"); 
-    
-    // messages
+    printf("SERVER: client accepted\n");
 
-    WSACleanup(); 
+    if (send(clientSocket, message, messageLen, 0) == SOCKET_ERROR) {
+        printf("SERVER_ERROR: %d\n", WSAGetLastError());
+        return 1;
+    }
+
+    printf("SERVER: message sent\n");
+
+    if (recv(clientSocket, recvBuffer, sizeof(recvBuffer), 0) == SOCKET_ERROR) {
+        printf("SERVER_ERROR: %d\n", WSAGetLastError());
+        return 1;
+    }
+
+    printf("SERVER: received message: %s\n", recvBuffer);
+
+    // close and cleanup
+    closesocket(clientSocket);
+    closesocket(serverSocket);
+    WSACleanup();
     getchar(); 
-    return 0; 
+    return 0;
 }
