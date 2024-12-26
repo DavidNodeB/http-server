98f6dea96af8302fe5071afa4613c5e8cb065201
DavidNodeB <db1416db@gmail.com>
2024-12-26 12:07:04 -0500

    Added simple parser

--- src/socket.c
+++ src/socket.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <winsock2.h>
 #include <string.h>
+#include <stdlib.h> 
 
 int main(int argc, char *argv[]) {
     WSADATA wsaData;
@@ -14,6 +15,7 @@ int main(int argc, char *argv[]) {
     char recvBuffer[512];
     int clientSize = sizeof(clientAddr);
     int port = 5001;
+    char *token, *method, *path, *version, *connection, *host, *acceptstr; 
 
     if (WSAStartup(ws2Version, &wsaData) != 0) {
         printf("SERVER_ERROR: %d\n", WSAGetLastError());
@@ -68,6 +70,38 @@ int main(int argc, char *argv[]) {
     }
 
     printf("%s\n", recvBuffer);
+
+    token = strdup(recvBuffer); 
+    method = strtok(recvBuffer, " "); 
+    path = strtok(NULL, " "); 
+    version = strtok(NULL, " "); 
+    host = strstr(token, "Host: ");
+    acceptstr = strstr(token, "Accept: "); 
+    connection = strstr(token, "Connection: "); 
+
+    if (token != NULL) {
+        // skip over identifier
+        host += 6;
+        acceptstr += 8;
+        connection += 12; 
+        
+        // find the end of the line
+        char* hostend = strpbrk(host, " \r\n"); 
+        char* acceptend = strpbrk(acceptstr, ",");
+        char* connectionend = strpbrk(connection, " ");
+        
+        // terminate the line
+        *hostend = '\0'; 
+        *acceptend = '\0'; 
+        *connectionend = '\0';        
+    } 
+    
+    printf("%s\n", method); 
+    printf("%s\n", path); 
+    printf("%s\n", version); 
+    printf("%s\n", host); 
+    printf("%s\n", acceptstr); 
+    printf("%s\n", connection);  
 
     // close and cleanup
     closesocket(clientSocket);
